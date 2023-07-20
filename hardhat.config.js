5731e7bd30a87f976efedf82736ce1b32bcebc88
intrepid <tony.kp.lau@gmail.com>
2023-07-20 15:25:06 -0400

    whitelist contract deployment on hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,29 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({path:".env"});
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const CONFLUX_API_KEY_URL = process.env.CONFLUX_API_KEY_URL;
+const CONFLUX_PRIVATE_KEY = process.env.CONFLUX_PRIVATE_KEY;
+
 module.exports = {
-  solidity: "0.8.19",
+  solidity: "0.8.4",
+  networks: {
+    conflux: {
+      chainID: 71,
+      url: CONFLUX_API_KEY_URL,
+      accounts: [CONFLUX_PRIVATE_KEY],
+    },
+  },
 };
+
+// conflux: {
+//   chainId: 1030,
+//   url: "https://evm.confluxrpc.com",
+//   accounts:["privateKey"],
+
+
+// require("@nomicfoundation/hardhat-toolbox");
+
+// /** @type import('hardhat/config').HardhatUserConfig */
+// module.exports = {
+//   solidity: "0.8.19",
+// };
