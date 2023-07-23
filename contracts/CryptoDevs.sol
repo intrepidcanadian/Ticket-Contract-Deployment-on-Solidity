535aa8db1d5a4f879fe81d01dfe685405e5a8601
intrepid <tony.kp.lau@gmail.com>
2023-07-22 20:08:01 -0400

    contracts used

---
+++ contracts/CryptoDevs.sol
@@ -0,0 +1,7 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.4;
+
+// import ERC721Enumerable from "./ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./Whitelist.sol";
