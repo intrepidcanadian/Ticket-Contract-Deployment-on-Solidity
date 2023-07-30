453561ad1d22da81f3c028f4e5774a0c92fde616
intrepid <tony.kp.lau@gmail.com>
2023-07-30 10:43:28 -0400

    flattened sol

--- contracts/IWhitelist.sol
+++ contracts/IWhitelist.sol
@@ -4,6 +4,3 @@ pragma solidity ^0.8.4;
 interface IWhitelist {
     function whitelistedAddresses(address) external view returns (bool);
 }
-
-
-
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,50 +1,28 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-
-// const { ethers } = require("hardhat");
-
-// const gasLimit = 10000000;
-
-// async function main() {
-//   const whitelistContract = await ethers.getContractFactory("Whitelist");
-//   const deployedWhitelistContract = await whitelistContract.deploy(10, {
-//     gasLimit: gasLimit,
-//   });
-//   await deployedWhitelistContract.deployed();
-//   console.log("Whitelist Contract Address", deployedWhitelistContract.address);
-// }
-
-// main()
-//   .then(()=> process.exit(0))
-//   .catch ((error) => {
-//   console.error(error);
-//   process.exit(1);
-// });
-//determine how to link to hardhat library
-//why do i need ethers
-
-
-
 const hre = require("hardhat");
+require("dotenv").config({ path: ".env" });
+const { WHITELIST_CONTRACT_ADDRESS, METADATA_URL } = require("../constants");
 
 async function main() {
+  // Address of the whitelist contract that you deployed in the previous module
+  const whitelistContract = WHITELIST_CONTRACT_ADDRESS;
+  // URL from where we can extract the metadata for a Crypto Dev NFT
+  const metadataURL = METADATA_URL;
   /*
   DeployContract in ethers.js is an abstraction used to deploy new smart contracts,
-  so whitelistContract here is a factory for instances of our Whitelist contract.
+  so cryptoDevsContract here is a factory for instances of our CryptoDevs contract.
   */
-  // here we deploy the contract
-const whitelistContract = await hre.ethers.deployContract("Whitelist", [10]);
-// 10 is the Maximum number of whitelisted addresses allowed
 
-// wait for the contract to deploy
-await whitelistContract.waitForDeployment();
+ // here we deploy the contract
+ const cryptoDevsContract = await hre.ethers.deployContract("CryptoDevs", [
+   metadataURL,
+   whitelistContract
+ ]);
 
-// print the address of the deployed contract
-console.log("Whitelist Contract Address:", whitelistContract.target);
+ // wait for the contract to deploy
+ await cryptoDevsContract.waitForDeployment();
+
+ // print the address of the deployed contract
+ console.log("Crypto Devs Contract Address:", cryptoDevsContract.target);
 }
 
 // Call the main function and catch if there is any error
@@ -54,36 +32,3 @@ main()
     console.error(error);
     process.exit(1);
   });
-
-
-
-
-
-
-// const hre = require("hardhat");
-
-// async function main() {
-//   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-//   const unlockTime = currentTimestampInSeconds + 60;
-
-//   const lockedAmount = hre.ethers.parseEther("0.001");
-
-//   const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-//     value: lockedAmount,
-//   });
-
-//   await lock.waitForDeployment();
-
-//   console.log(
-//     `Lock with ${ethers.formatEther(
-//       lockedAmount
-//     )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-//   );
-// }
-
-// // We recommend this pattern to be able to use async/await everywhere
-// // and properly handle errors.
-// main().catch((error) => {
-//   console.error(error);
-//   process.exitCode = 1;
-// });
